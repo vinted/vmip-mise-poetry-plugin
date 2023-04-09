699b04fd901c86128051bf02ae28283c31afcd92
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-04-08 19:23:03 -0500

    shfmt

--- bin/utils.sh
+++ bin/utils.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 
-if [ "${RTX_TRACE:-}" = "1" ]; then
-    set -x
+if [ "${RTX_TRACE-}" = "1" ]; then
+  set -x
 fi
 
 echoerr() {
-    echo "$1" >&2
+  echo "$1" >&2
 }
 
 poetry_bin() {
-    echo "$RTX_INSTALL_PATH/bin/poetry"
+  echo "$RTX_INSTALL_PATH/bin/poetry"
 }
 
 poetry_venv() {
-    local pyproject
-    pyproject="$(eval "echo ${RTX_TOOL_OPTS__PYPROJECT-}")"
-    if [ "$pyproject" = "" ]; then
-        return
-    fi
-    if [[ "$pyproject" != /* ]] && [[ -n "${RTX_PROJECT_ROOT-}" ]]; then
-        pyproject="${RTX_PROJECT_ROOT-}/$pyproject"
-    fi
-    if [[ ! -f "$pyproject" ]]; then
-        echoerr "rtx-poetry: No pyproject.toml found. Executing \`poetry init\` to create \`$pyproject\` first."
-        poetry init
-    fi
-    "$(poetry_bin)" env info --path 2>/dev/null
-    true
+  local pyproject
+  pyproject="$(eval "echo ${RTX_TOOL_OPTS__PYPROJECT-}")"
+  if [ "$pyproject" = "" ]; then
+    return
+  fi
+  if [[ $pyproject != /* ]] && [[ -n ${RTX_PROJECT_ROOT-} ]]; then
+    pyproject="${RTX_PROJECT_ROOT-}/$pyproject"
+  fi
+  if [[ ! -f $pyproject ]]; then
+    echoerr "rtx-poetry: No pyproject.toml found. Executing \`poetry init\` to create \`$pyproject\` first."
+    poetry init
+  fi
+  "$(poetry_bin)" env info --path 2>/dev/null
+  true
 }
